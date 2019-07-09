58b9693df7d54fd8c38fb5a9d41775375a067145
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-07-08 17:41:27 -0700

    Implement blog posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux';
+import postsReducers from './postsReducer';
 
-export default combineReducers({});
+export default combineReducers({
+  posts: postsReducers,
+});
